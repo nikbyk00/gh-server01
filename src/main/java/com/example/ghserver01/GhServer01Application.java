cae10ce8c0e1d2142f6f7d76a02ce6ea759e1be7
nikita <nb85294@gmail.com>
2023-04-12 15:28:16 +0300

    nb85294@gmail.com

--- src/main/java/com/example/ghserver01/GhServer01Application.java
+++ src/main/java/com/example/ghserver01/GhServer01Application.java
@@ -2,12 +2,24 @@ package com.example.ghserver01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GhServer01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(GhServer01Application.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
 }
